4f75e6a6acad942acbdb2e7cdcbfad268b049158
CRich96 <chris575502@icloud.com>
2023-11-14 08:50:23 -0600

    updates

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,10 +1,10 @@
 const router = require('express').Router();
 
-const apiRoutes = require('./api');
+const apiRoutes = require('../api');
 
-const homeRoutes = require('./home-routes.js');
-const postRoutes = require('./post-routes');
-const commentRoutes = require('./comment-routes');
+const userRoutes = require('../api/user-routes');
+const postRoutes = require('../api/post-routes');
+const commentRoutes = require('../api/comment-routes');
 
 router.use('/users', userRoutes);
 router.use('/posts', postRoutes);
